f96d208a056c22c6099bc417ba5b81dde13defdb
Shane Nelson <swiftyshane@gmail.com>
2018-01-23 19:55:43 -0500

    Changes to gradient layer

--- GradientHandler/ViewController.swift
+++ GradientHandler/ViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-  override func viewDidLoad() {
+class ViewController: UIViewController
+{
+  
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    GradientApplier.apply(to: view)
+    
   }
 
-  override func didReceiveMemoryWarning() {
+  override func didReceiveMemoryWarning()
+  {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
